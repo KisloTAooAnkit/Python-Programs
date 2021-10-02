e14a3caa697e66fb9a5100954ec8e7cb1959e921
KisloTAooAnkit <40890260+KisloTAooAnkit@users.noreply.github.com>
2021-10-02 11:39:10 +0530

    dell

--- Trees/LCA.py
+++ Trees/LCA.py
@@ -30,17 +30,34 @@ class Solution:
         return Package(root,False)
     
 
+    def findLCASpaceOpt(self,root,p,q):
+        if not root:
+            return None
+
+        left = self.findLCASpaceOpt(root.left,p,q)
+        right = self.findLCASpaceOpt(root.right,p,q)
+        
+        if left and right:
+            return root
+        if root.data == q or root.data == p:
+            return root
+        if left:
+            return left
+        if right:
+            return right
+        return None
+    
     
     def lowestCommonAncestor(self, root, p, q):
-        ans = self.findLCA(root,p,q)
-        return ans.node.val
+        ans = self.findLCASpaceOpt(root,p,q)
+        return ans.val
 p =5
-q = 4
+q = 1
 null = "null"
 a = [3,5,1,6,2,0,8,null,7,4]
 
 root = BuildTreeFromStr.buildTree(a)
-#PrintTree.print2D(root)
+PrintTree.print2D(root)
 obj = Solution()
 print(obj.lowestCommonAncestor(root,p,q))
 
--- Trees/boundaryTraversal.py
+++ Trees/boundaryTraversal.py
@@ -1,37 +1,14 @@
 import BuildTreeFromStr
-def RightDFS(root,ans,level):
-    if not root:
-        return
-    if level == len(ans):
-        ans.append(root.data)
-    RightDFS(root.right,ans,level+1)
-    RightDFS(root.left,ans,level+1)
+from collections import deque
 
-
-def DFS(root,ans,level):
-    if not root.left and not root.right:
-        ans.append(root.data)
-        return
-    if len(ans) == level:
-        ans.append(root.data)
-    
-    if root.left:
-        DFS(root.left,ans,level+1)
-    if root.right:
-        DFS(root.right,ans,level+1)
-    return
 
 def printBoundary(root):
     ans = []
-    DFS(root,ans,0)
-    ans.pop(-1)
-    newAns = []
-    print("before",ans)
-    RightDFS(root,newAns,0)
-    print("after",newAns)
-    newAns = newAns[::-1]
-    newAns.pop()
-    return ans + newAns
+    q = deque()
+    q.append(root)
+    q.append(None)
+    while(len(q)>1):
+        val = q.pop()
 
 N = "null"
 null = "null"
