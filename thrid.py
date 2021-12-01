828a7c4189680effd2aaf70ac6eaab05460904b7
Ankit Singh <ve00ym158@globalymc.com>
2021-12-01 08:44:44 +0530

    Mac

--- thrid.py
+++ thrid.py
@@ -1,27 +1,24 @@
-from collections import deque
-from collections import defaultdict
-def sol(n,meetings,firstPerson):
-    dic = defaultdict(list)
-    for meeting in meetings:
-        x,y,_ = meeting
-        dic[x].append(y)
-        dic[y].append(x)
-    q = deque()
-    q.append(firstPerson)
-    ans = [0]
-    hashmap = dict()
-    while q:
-        person = q.pop()
-        hashmap[person] = 1
-        for i in dic[person]:
-            if i not in hashmap:
-                q.append(i)
-        if person != 0:
-            ans.append(person)
+def largestMatrix(grid):
+    rows = len(grid)
+    cols = len(grid[0])
+    one = "1"
+    zero = "0"
+    dp = [[0]*cols for _ in range(rows)]
+
+    ans = 0
+    for i in range(cols):
+        if grid[0][i] == one:
+            dp[0][i] = 1
+            ans = 1
+    
+    for i in range(rows):
+        if grid[i][0] == one:
+            dp[i][0] = 1
+            ans = 1
+    for i in range(1,rows):
+        for j in range(1,cols):
+            if grid[i][j] == 1:
+                dp[i][j] = 1 + min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])
+                ans = max(ans,dp[i][j])
     return ans
 
-arr =  [[3,4,2],[1,2,1],[2,3,1]]
-firstPerson = 1
-print(sol(len(arr),arr,firstPerson))
-
-        
