a770a7427f90973a731a14aa6c459bc8efa6eb43
KisloTAooAnkit <40890260+KisloTAooAnkit@users.noreply.github.com>
2022-01-05 08:49:50 +0530

    Update 1567. Maximum Length of Subarray With Positive Product.py

--- DP2/1567. Maximum Length of Subarray With Positive Product.py
+++ DP2/1567. Maximum Length of Subarray With Positive Product.py
@@ -1,61 +1,60 @@
-def getMaxLen(arr):
+def maxLen(arr):
     n = len(arr)
-    for i in range(n):
-        if arr[i] ==0:
-            continue
-        arr[i]//=arr[i]
-    posL = 1
-    negL = 1
-    pos,neg = arr[0],arr[0]
-
-    maxL = 0
+    dp = [[0]*n for _ in range(2)]
+    
+    if arr[0] < 0:
+        dp[0][0] = 0
+        dp[1][0] = 1
+    elif arr[0] > 0:
+        dp[0][0] = 1
+        dp[1][0] = 0
+ 
+    ans = 0
     for i in range(1,n):
-
         val = arr[i]
-        currPos,currNeg = 0,0
-        cpl,cnl = 1,1
-        if val < 0:
-            #pp
-            if val*neg >= val:
-                currPos = val*neg
-                cpl = negL+1
-            else:
-                currPos = val
-
-            #np
-            if val*pos <= val:
-                currNeg = val*pos
-                cnl = posL+1
-            else:
-                currNeg = val 
-            
-        if val >0:
-            if val*pos >= val:
-                currPos = val*pos
-                cpl = posL +1
-            else:
-                currPos = val
-
-            if val*neg <= val:
-                currNeg = val*neg
-                cnl = negL+1
-            else:
-                currNeg = val
         
-        if val == 0:
-            currPos = 0
-            currNeg = 0
-        pos = currPos
-        neg = currNeg
-        posL = cpl
-        negL = cnl
-        if currPos > 0:
-            maxL = max(maxL,cpl)
-        if currNeg >0:
-            maxL = max(maxL,cnl)
+        if val < 0 :
+            dp[0][i] = 0 if dp[1][i-1] == 0 else dp[1][i-1] +1
+            dp[1][i] = 1 if dp[0][i-1] == 0 else dp[0][i-1] +1
 
-    return maxL
+        if val > 0:
+            dp[0][i] = 1 if dp[0][i-1] == 0 else dp[0][i-1] +1
+            dp[1][i] = 0 if dp[1][i-1] == 0 else dp[1][i-1] +1
+
+        ans = max(ans,dp[0][i])
+    return ans
+
+arr = [0,1,-2,-3,-4]
+print(maxLen(arr))
 
 
-arr =[-1,-2,-3,0,1]
-print(getMaxLen(arr))
+
+############## O(1) Space Logic is same ############################
+def getMaxLen(arr) -> int:
+    n = len(arr)
+    pp = 0
+    pn = 0
+    ans = 0
+    if arr[0] < 0:
+        pp = 0
+        pn = 1
+    elif arr[0] > 0:
+        ans = 1
+        pp = 1
+        pn = 0
+
+    
+    for i in range(1,n):
+        val = arr[i]
+        cp,cn = 0,0
+        if val < 0 :
+            cp = 0 if pn == 0 else pn +1
+            cn = 1 if pp == 0 else pp +1
+
+        if val > 0:
+            cp = 1 if pp == 0 else pp +1
+            cn = 0 if pn == 0 else pn +1
+
+        ans = max(ans,cp)
+        pp,pn = cp,cn
+    return ans
