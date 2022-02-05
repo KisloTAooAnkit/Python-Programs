abc92f5035c96860684a61032dd560c84f31a9ea
Ankit Singh <ve00ym158@globalymc.com>
2022-02-05 18:47:22 +0530

    Mac

--- thrid.py
+++ thrid.py
@@ -1,63 +1,34 @@
-import heapq
-class Item:
-    
-    def __init__(self,price,x,y,d) -> None:
-        self.x = x
-        self.y = y
-        self.d = d 
-        self.price = price
-        
-    
-    def __gt__(self,other):
-        
-        if self.d != other.d:
-            return self.d < other.d
-        elif self.price != other.price:
-            return self.price < other.price
-        elif self.x != other.x:
-            return self.x < other.x
-        else:
-            return self.y < other.y
-    
-    
-    
-def dfs(visited,grid,x,y,n,m,k,pq,d,pricing):
-    if x>=n or x<0 or y>=n or y<0:
-        return
-    if visited[x][y] or grid[x][y] == 0:
-        return 
-    visited[x][y] = True
-    if pricing[0] <= grid[x][y] <= pricing[1]:
-        if len(pq) == k:
-            heapq.heappushpop(pq,Item(grid[x][y],x,y,d))
-        else:
-            heapq.heappush(pq,Item(grid[x][y],x,y,d))
 
-    dfs(visited,grid,x+1,y,n,m,k,pq,d+1,pricing)
-    dfs(visited,grid,x,y+1,n,m,k,pq,d+1,pricing)
-    dfs(visited,grid,x-1,y,n,m,k,pq,d+1,pricing)
-    dfs(visited,grid,x,y-1,n,m,k,pq,d+1,pricing)
-    return
+from typing import Counter
 
-def solve(grid,pricing,start,k):
-    n = len(grid)
-    m = len(grid[0])
-    v = [[False]*m for _ in range(n)]
-    pq = []
-    dfs(v,grid,start[0],start[1],n,m,k,pq,0,pricing)
-    
+
+def solve(s,p):
+    dic = Counter(p)
+    c = 0
+    end = 0
+    start = 0
     ans = []
-    while(pq):
-        val = heapq.heappop(pq)
-        ans.append([val.x,val.y])
-    ans.sort()
+    n = len(s)
+    m = len(p)
+    while(end < n):
+        
+        if s[end] in dic:
+            dic[s[end]] -=1
+            if dic[s[end]] >= 0:
+                c +=1    
+
+        while(end-start +1 == m):
+            if c == m:
+                ans.append(start)
+            
+            if s[start] in dic:
+                dic[s[start]] +=1
+                if dic[s[start]] > 0:
+                    c -=1
+            start +=1
+        end +=1
     return ans
+s = "abab"
+p = "ab"
+print(solve(s,p))
 
-
-grid = [[1,2,0,1],
-        [1,3,0,1],
-        [0,2,5,1]]
-pricing = [2,5]
-start = [0,0]
-k = 3
-print(solve(grid,pricing,start,k))
